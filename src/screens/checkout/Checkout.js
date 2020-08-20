9629f348470cfc26ff17128c5a283272420328d0
swatitoshniwal <swati.toshniwal2@gmail.com>
2020-08-20 22:45:13 +0530

    Added delivery and payment steps

--- src/screens/checkout/Checkout.js
+++ src/screens/checkout/Checkout.js
@@ -4,6 +4,11 @@ import Grid from '@material-ui/core/Grid';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import { withStyles } from '@material-ui/core/styles';
+import Step from '@material-ui/core/Step';
+import Stepper from '@material-ui/core/Stepper';
+import StepLabel from '@material-ui/core/StepLabel';
+import StepContent from '@material-ui/core/StepContent';
+import AppBar from '@material-ui/core/AppBar';
 // import Header
 
 const styles = theme => ({
@@ -12,30 +17,51 @@ const styles = theme => ({
     }
 });
 
+function getSteps() {
+    return ['Delivery', 'Payment'];
+};
+
 class Checkout extends Component {
     constructor(){
     super();
     this.state = {
+        activeStep:0,
 
     };
 }
 
     render(){
         const { classes } = this.props;
-
+        const steps = getSteps();
+        
         return (
             <div>
-                {/* <Header/> */}
-                    <Grid container>
-                        <Grid item>
-                            <div>
-                                <Tabs value={0}>
-                                    <Tab label='EXISTING ADDRESS' />
-                                    <Tab label='NEW ADDRESS' />
-                                </Tabs>
-                            </div>
-                        </Grid>
-                    </Grid>
+            {/* <Header/> */}
+            <Grid container= {true}>
+                <Grid item>
+                    <div>
+                        <Stepper activeStep={0} orientation='vertical'>
+                            {steps.map((label, index) => (
+                                <Step key={label}>
+                                    <StepLabel>{label}</StepLabel>
+                                    <StepContent>
+                                        {index === 0 ?  
+                                        <div className={classes.tabRoot}>
+                                            <AppBar position='static'>
+                                                <Tabs value={0}>
+                                                    <Tab label='EXISTING ADDRESS' />
+                                                    <Tab label='NEW ADDRESS' />
+                                                </Tabs>
+                                            </AppBar>
+                                        </div>
+                                        :''}
+                                    </StepContent>
+                                </Step>
+                            ))}
+                        </Stepper>
+                    </div>
+                </Grid>
+            </Grid>
             </div>
         );
     }

3aae7c1d6dfacff5784f63006518590623264882
Satyanjal <satyanjal.prakash@analyttica.com>
2020-08-22 14:14:27 +0530

    UI of Header View is completed

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './Header.css';
 import SearchBar from '../searchBar/searchBar';
 import FastfoodIcon from '@material-ui/icons/Fastfood';
+import Button from '@material-ui/core/Button';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
 
 const Header = function (props) {
@@ -11,6 +13,9 @@ const Header = function (props) {
             <FastfoodIcon className="fast_food"/>
             <span className="search_btn">
                 <SearchBar/>
+            </span>
+            <span className="login_btn">
+                <Button variant="contained"><AccountCircleIcon/> LOGIN</Button>
             </span>
         </div>
     )

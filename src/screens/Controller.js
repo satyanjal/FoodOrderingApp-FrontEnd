e295225f8562f57b768e84b38356f5485022a0cd
parulchoudhary-dse <parulchoudhary.dse@gmail.com>
2020-08-30 16:57:09 +0530

    Home Page Implemented

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -1,20 +1,29 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Home from '../screens/home/Home';
+import React,{Component} from 'react';
+import {BrowserRouter as Router,Route} from 'react-router-dom';
+import Home from './home/Home';
+import Details from './details/Details';
 
 
 
-class Controller extends Component {
+//Creating controller class for easy routing the pages
+class Controller extends Component{
+    constructor(){
+        super()
+        this.baseUrl = "http://localhost:3000/api/" //setting the baseUrl of the api
+    }
 
-  render() {
-    return (
-      <Router>
-        <div className="main-container">
-          <Route exact path='/home' render={(props) => <Home {...props}/>} />
-        </div>
-      </Router>
-    )
-  }
+    render(){
+        return(
+            <Router>
+                <div className = 'main-container'>
+                    <Route exact path = '/' render={(props) => <Home {...props} baseUrl = {this.baseUrl}/>}/> {/* Route to home Page */ }
+                    <Route path='/restaurant/:id' render={(props) => <Details {...props} baseUrl={this.baseUrl} />} /> {/* Route to restaurant details Page */}
+                </div>
+            </Router>
+
+        )
+    }
+
 }
 
 export default Controller;
